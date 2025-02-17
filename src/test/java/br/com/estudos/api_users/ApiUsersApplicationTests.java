8bf4d5a236e8d5a80e2f9530ef5a65da6e897ea7
Guilherme Romão <guilhermersimoes87@gmail.com>
2025-02-17 07:48:09 -0300

    initial commit

---
+++ src/test/java/br/com/estudos/api_users/ApiUsersApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.estudos.api_users;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiUsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
